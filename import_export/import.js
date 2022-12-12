36363490272f186e55eeba008aafcf221b0f44e0
mukeshrao-cesit <110102272+mukeshrao-cesit@users.noreply.github.com>
2022-12-12 11:41:40 +0530

    Delete import_export directory

--- import_export/import.js
+++
@@ -1,7 +0,0 @@
-// importing the functions and object of the functions from export.js file
-
-import MyItem, { getItem, setItem } from "./export.js";
-
-console.log(getItem());
-console.log(setItem("mukesh"));
-console.log(MyItem.getItem());
