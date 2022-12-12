20824f7124b20e4899621cad164bdf6492a72ff1
Mukesh Rao Nagendrarao <mukeshrao.nagendrarao@cesltd.com>
2022-12-12 11:29:18 +0530

    vite-react

---
+++ Vite-React/sample-vite-app/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  const styles = {
+    color: "red",
+    fontFamily: "cursive",
+  };
+  return (
+    <div>
+      <h1 style={styles}>Hello all...!, I'm Mukesh Rao</h1>
+    </div>
+  );
+}
+
+export default App;
