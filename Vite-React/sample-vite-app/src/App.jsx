542197b0756d19fc0f504170e249c1093af42eee
Mukesh Rao Nagendrarao <mukeshrao.nagendrarao@cesltd.com>
2022-12-13 13:08:53 +0530

    webpage header update

--- Vite-React/sample-vite-app/src/App.jsx
+++ Vite-React/sample-vite-app/src/App.jsx
@@ -1,12 +1,9 @@
-import ClassComp from "./ClassComp";
-import { Header } from "./Header";
-import { Body } from "./Body";
-import { Footer } from "./Footer";
+import { Header } from "./Header/Header";
+import { Body } from "./Body/Body";
+import { Footer } from "./Footer/Footer";
 function App() {
   return (
     <div>
-      {/* <h1>I'm function component</h1> */}
-      {/* <ClassComp /> */}
       <Header />
       <Body />
       <Footer />
