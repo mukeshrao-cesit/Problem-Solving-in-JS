95e0de8be79a6899499e8a92f0fb660967d2cd70
Mukesh Rao Nagendrarao <mukeshrao.nagendrarao@cesltd.com>
2022-12-14 15:26:06 +0530

    changes files to another folder

--- Vite-React/sample-vite-app/src/Body/Body.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Banner1 } from "./Banner1";
-export const Body = () => {
-  return (
-    <div>
-      <Banner1 />
-    </div>
-  );
-};
--- Vite-React/sample-vite-app/src/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Footer = () => {
-  return (
-    <div>
-      <h1 style={{ color: "white", backgroundColor: "green" }}>Im footer</h1>
-    </div>
-  );
-};
